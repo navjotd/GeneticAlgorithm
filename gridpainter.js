2c071b12b26a7ff6361c04d939e0fccad5d2ee14
nav <navjot.dznr@gmail.com>
2014-12-26 00:48:06 -0500

    added gh pages branch

---
+++ gridpainter.js
@@ -0,0 +1,118 @@
+var redToGreen = ["FF0000",
+"FF1100",
+"FF2200",
+"FF3300",
+"FF4400",
+"FF5500",
+"FF6600",
+"FF7700",
+"FF8800",
+"FF9900",
+"FFAA00",
+"FFBB00",
+"FFCC00",
+"FFDD00",
+"FFEE00",
+"FFFF00",
+"EEFF00",
+"DDFF00",
+"CCFF00",
+"BBFF00",
+"AAFF00",
+"99FF00",
+"88FF00",
+"77FF00",
+"66FF00",
+"55FF00",
+"44FF00",
+"33FF00",
+"22FF00",
+"11FF00",
+"00FF00"]
+
+
+function GridPainter(canvasNode, pixelSize) {
+	this.node = canvasNode;
+	this.pixelSize = pixelSize;
+	this.node.width = window.innerWidth - 100;
+	this.node.height = window.innerHeight - 100;
+	this.context = this.node.getContext("2d");
+	this.context.strokeStyle = '#070707';
+}
+
+GridPainter.prototype.updateSize = function() {
+	this.node.width = window.innerWidth - 100;
+	this.node.height = window.innerHeight - 100;
+	this.context = this.node.getContext("2d");
+	this.context.strokeStyle = '#070707';
+}
+
+GridPainter.prototype.paintGrid = function() {
+
+	for (var i = 0.5; i < this.node.width/this.pixelSize; i++) {
+		var startPoint = this.pixelSize*i;
+		this.context.moveTo(startPoint, 0);
+		this.context.lineTo(startPoint, this.node.height);
+	}
+
+	for (var i = 0.5; i < this.node.height/this.pixelSize; i++) {
+		var startPoint = this.pixelSize*i;
+		this.context.moveTo(0, startPoint);
+		this.context.lineTo(this.node.width, startPoint);
+	}
+
+	this.context.stroke();
+}
+
+GridPainter.prototype.clear = function() {
+	this.context.clearRect(0,0,this.node.width, this.node.height);
+}
+
+function PointPainter(canvasNode) {
+	this.node = canvasNode;
+	this.node.width = window.innerWidth - 100;
+	this.node.height = window.innerHeight - 100;
+	this.context = this.node.getContext("2d");
+	this.points = [];
+}
+
+PointPainter.prototype.updateSize = function() {
+	this.node.width = window.innerWidth - 100;
+	this.node.height = window.innerHeight - 100;
+	this.context = this.node.getContext("2d");
+}
+
+PointPainter.prototype.clear = function() {
+	this.context.clearRect(0,0,this.node.width, this.node.height);
+}
+
+PointPainter.prototype.paintPoints = function(points) {
+	if (!points) 
+		var points = this.points;
+	else
+		this.points = points;
+	this.context.fillStyle = "white";
+	var originx = this.node.width/2;
+	var originy = this.node.height/2;
+
+	for (var i = 0; i < points.length; i++) {
+		var rflctx = (Math.floor(Math.random() * 2))? 1: -1;
+		var rflcty = (Math.floor(Math.random() * 2))? 1: -1;
+		var r = Math.abs(TARGET - points[i].decode());
+		if (r > Math.min(this.node.width/2 - 50, this.node.width/2 - 50)) 
+			r = Math.min(this.node.width/2 - 50, this.node.width/2 - 50);
+		this.context.fillStyle = getPointColour(r);
+		var x = Math.random() * r * rflctx;
+		var y = Math.sqrt(Math.abs((r*r) - (x * x))) * rflcty;
+		//console.log("val: " + points[i].decode() + " r: " + r + " x: " + x + "y: " + y);
+		this.context.fillRect(originx + (x * 2), originy + (y * 2), 2, 2);
+	}
+}
+
+function getPointColour(r) {
+	//debugger;
+	if (r < 31)
+		return '#' + redToGreen[Math.floor(r)];
+	else
+		return '#333';
+}
---
+++ main.js
@@ -0,0 +1,104 @@
+function setSettings() {
+	var popSize = document.getElementById('popSize').value;
+	var target = document.getElementById('target').value;
+	var mutation = document.getElementById('mutation').value;
+
+	if (popSize % 2 === 0)
+		POP_SIZE = popSize;
+
+	TARGET = target;
+	MUTATION_RATE = mutation;
+
+	document.getElementById('popSize').value = "";
+	document.getElementById('target').value = "";
+	document.getElementById('mutation').value = "";
+}
+
+function hideMenu() {
+	document.getElementById('settings').className = "hide";
+}
+
+function showMenu() {
+	document.getElementById('settings').className = "show";
+}
+
+function displayResults(result) {
+	var results = document.getElementById('results');
+	results.className = "show";
+	results.innerHTML = result;
+}
+
+function hideResults() {
+	document.getElementById('results').className = "hide";
+}
+
+hideResults();
+
+var gridPainter = new GridPainter(document.getElementById('grid'), 5);
+var pointPainter = new PointPainter(document.getElementById('points'));
+
+window.onresize = function() {
+	gridPainter.clear();
+	gridPainter.updateSize();
+	gridPainter.paintGrid();
+	pointPainter.clear();
+	pointPainter.updateSize();
+	pointPainter.paintPoints();
+}
+
+gridPainter.paintGrid();
+
+function start() {
+	var counter = 0;
+	var p = new Population();
+	p.initialize(POP_SIZE);
+	p.setFitnessScores();
+
+	// while(p.matches.length === 0) {
+	// 	p.evolve();
+	// 	counter++;
+	// 	gridPainter.paintPoints(true);
+	// 	gridPainter.paintGrid();
+	// 	grid.setCells(p);
+	// 	gridPainter.paintPoints();
+	// 	debugger;
+	// 	var vals = [];
+	// 	p.forEach(function(item) { vals.push(item.decode()); })
+	// 	vals.sort(function(a,b){ return (a < b) });
+	// 	console.log(vals);
+	// }
+
+	var id = setInterval(function(){
+		if (p.matches.length > 0) {
+			clearInterval(id);
+			console.log(counter);
+			console.log(p.matches[0]);
+			debugger;
+			displayResults(p[p.matches[0]].sequence);
+		}
+		pointPainter.clear();
+		pointPainter.paintPoints(p);
+		// var vals = [];
+		// p.forEach(function(item) { vals.push(item.decode()); })
+		// vals.sort(function(a,b){ return (a < b) });
+		// console.log(vals);
+		p.evolve();
+		counter++;
+	}, 100);
+}
+
+var button =  document.getElementById('button');
+
+button.onclick = function(e) {
+	setSettings();
+	hideMenu();
+	start();
+}
+
+document.onkeyup = function(e) {
+	e.wich = e.wich || e.keyCode;
+	if (e.wich === 13 && document.getElementById('results').className === "show") {
+		hideResults();
+		showMenu()
+	}
+}
